daeca46b89e73b89e9097e268765a35a3b2ba708
Sho <emesarke@yahoo.co.jp>
2015-07-10 01:49:27 +0900

    ・各グループのアイコンが表示されるようになった ・ドロップボックスに保存したファイル名をサーバー側で管理するようにした ・写真を撮ると直接写真がmapに貼られるのでは無くアイコンが 　設置されるように変更した

--- app/src/main/java/jp/co/thinkethbank/kurikita/chisanpo/BitmapEditor.java
+++ app/src/main/java/jp/co/thinkethbank/kurikita/chisanpo/BitmapEditor.java
@@ -7,6 +7,7 @@ import java.io.ByteArrayOutputStream;
 
 /** ビットマップに対する加工を行う */
 public class BitmapEditor {
+    private static final float THUMB_SIZE = 64f;
     /**
      * Bitmapをリサイズする
      * @param src スケーリングするBitmap
@@ -27,6 +28,31 @@ public class BitmapEditor {
         matrix.postScale(scaleRate, scaleRate);
 
         return Bitmap.createBitmap(src, 0, 0, srcWidth, srcHeight, matrix, true);
+    }
+
+    static Bitmap thumbnail(Bitmap src){
+        int w = src.getWidth();
+        int h = src.getHeight();
+        int len;
+        int xOffset;
+        int yOffset;
+        float scaleRate;
+        Matrix matrix = new Matrix();
+
+        if(w > h){
+            len = h;
+            scaleRate = THUMB_SIZE / h;
+            xOffset = (w - h) / 2;
+            yOffset = 0;
+        }else{
+            len = w;
+            scaleRate = THUMB_SIZE / w;
+            xOffset = 0;
+            yOffset = (h - w) / 2;
+        }
+        matrix.postScale(scaleRate, scaleRate);
+
+        return Bitmap.createBitmap(src, xOffset, yOffset, len, len, matrix, true);
     }
 
     static byte[] compressJpeg(Bitmap bitmap, int quality){
